4943608c9e2264d7241e63cfd248b81db6d2c87d
AmrinderSinghKhangura <amrinder.khangura@edu.sait.ca>
2024-08-07 11:46:39 -0400

    changed page to Page

--- app/loginPage/page.js
+++ app/loginPage/page.js
@@ -1,48 +1,70 @@
-'use client'
-import 'tailwindcss/tailwind.css';
-import {useAuth} from "./_utils/auth_context";
-import {useState} from "react";
-import {useRouter} from "next/navigation";
+"use client";
+import "tailwindcss/tailwind.css";
+import { useAuth } from "./_utils/auth_context";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
+export default function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const { login } = useAuth();
+  const [error, setError] = useState(false);
+  const router = useRouter();
 
-export default function page() {
-  
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("");
-    const {login} = useAuth();
-    const [error, setError] = useState(false);
-    const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      setMessage("Login Successful... Redirecting to Dashboard");
+      setError(false);
+      setTimeout(() => {
+        router.push("/loginPage/mainpage");
+      }, 1000);
+    } catch (error) {
+      setMessage(
+        "Invalid username or Password Contact your Administrator for assistance"
+      );
+      setError(true);
+    }
+  };
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await login(email, password);
-            setMessage("Login Successful... Redirecting to Dashboard");
-            setError(false);
-            setTimeout(() => {
-                router.push("/loginPage/mainpage");
-            }, 1000);
-        } catch(error){
-            setMessage("Invalid username or Password Contact your Administrator for assistance")
-            setError(true);
-        }
-    };
-
-    return (
+  return (
     <>
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className='text-xl'>Formless</h1>
+      <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-xl">Formless</h1>
       </header>
-      <main className='bg-gray-200 flex justify-center items-center h-screen'>
-        <div className=' p-4 rounded-lg'>
-          <h2 className='text-2xl'>Welcome to Formless</h2>
-          <p className='text-lg'>Login</p>
-          <input type='text' className='border border-gray-300 p-2 rounded-lg w-full' placeholder='Username' value={email} onChange={(e) => setEmail(e.target.value)} required/>
-          <input type='password' className='border border-gray-300 p-2 rounded-lg w-full mt-2' placeholder='Password'value={password} onChange={(e) => setPassword(e.target.value)} required/>
-          <input type='submit' className='bg-gray-800 text-white p-2 rounded-lg w-full mt-2 hover:bg-orange-500 hover:text-black' onClick={handleSubmit} value='Login'/>
-          {message && <p className={error ? 'text-red-500' : 'text-green-500'}>{message}</p>}
+      <main className="bg-gray-200 flex justify-center items-center h-screen">
+        <div className=" p-4 rounded-lg">
+          <h2 className="text-2xl">Welcome to Formless</h2>
+          <p className="text-lg">Login</p>
+          <input
+            type="text"
+            className="border border-gray-300 p-2 rounded-lg w-full"
+            placeholder="Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            className="border border-gray-300 p-2 rounded-lg w-full mt-2"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input
+            type="submit"
+            className="bg-gray-800 text-white p-2 rounded-lg w-full mt-2 hover:bg-orange-500 hover:text-black"
+            onClick={handleSubmit}
+            value="Login"
+          />
+          {message && (
+            <p className={error ? "text-red-500" : "text-green-500"}>
+              {message}
+            </p>
+          )}
         </div>
       </main>
     </>
